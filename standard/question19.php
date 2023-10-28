292203a9ebd2492156ba4b2f4744531757d228a8
MMBAGA JOHNSON NATHANIEL <129178005+KaReeeeeeeeEM@users.noreply.github.com>
2023-10-28 17:39:23 +0100

    new

---
+++ standard/question19.php
@@ -0,0 +1,145 @@
+<?php 
+$score = $_GET['score'];
+$count = $_GET['count'];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>tQuiz</title>
+
+        <!-- add styles -->
+        <style>
+            *{
+                margin:0;
+                top:0;
+            }
+
+            body{
+                background-color: rgba(11, 95, 219, 0.973);
+            }
+
+            a{
+                text-decoration: none;
+            }
+
+            .nav{
+                display: flex;
+                justify-content: space-between;
+                padding-top:12px;
+                font-family: Arial, helvetica;
+            }
+
+            .nav .logo{
+                padding-left: 12px;
+                color: #fff;
+            }
+
+
+            ul{
+               list-style: none; 
+               display:flex;
+            }
+
+            ul li{
+                padding-left:8px;
+                padding-right:28px;
+                font-size:2rem;
+                color: #fff;
+            }
+
+            .game-body .intro-heading, .game-body .outro-heading{
+                font-family: cursive;
+                font-size:23px;
+                text-align: center;
+                padding-top:18px;
+                margin:23px auto 12px;
+                color: #fff;
+            }
+
+            .game-body .columns{
+                display: flex;
+                flex-direction: column;
+            }
+
+            .game-body .columns .row{
+                max-width: 50%;
+                width: 50% ;
+                height: 80px;
+                margin: 12px auto;
+                padding: 5px 8px;
+                text-align: center;
+                border-radius: 20px;
+                border:none;
+                box-shadow: 0px 6px #aaa;
+                background-color: #fff;
+                color: #000;
+                font-family: cursive;
+                font-size:24px;
+                font-weight:700;
+                cursor:pointer;
+                margin-bottom: 12px;
+            }
+
+            .game-body .columns .standard{
+                margin-top:70px;
+            }
+
+
+        </style>
+    </head>
+    <body>
+
+        <!-- Code for the navigation bar -->
+        <div class="nav">
+            <div class="logo">
+                <h1><a href="index.php" style="text-decoration:none;color:#fff;">tQuiz</a></h1>
+            </div>
+            <div class="navbar">
+                <ul>
+                    <li>Score = <?php echo $_GET['score'] ?></li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Code for the main body -->
+        <div class="game-body">
+            <div class="intro-heading">
+                <h1>What is the smallest state in USA?</h1>
+            </div>
+            
+            <div class="columns">
+                    <button class="row standard click" onclick='this.style.backgroundColor = "red"; var ran = Math.ceil(Math.random() * 50); window.location.href = `question${ran}.php?score=<?php echo $score ?>&count=<?php echo $count + 1 ?>`;'>New Jersey</button>
+                    <button class="row correct" id="correct" onclick="addScore()">Rhode island</button>
+                    <button class="row click" onclick='this.style.backgroundColor = "red"; var ran = Math.ceil(Math.random() * 50); window.location.href = `question${ran}.php?score=<?php echo $score ?>&count=<?php echo $count + 1 ?>`;'>Connecticut</button>
+                    <button class="row click" onclick='this.style.backgroundColor = "red"; var ran = Math.ceil(Math.random() * 50); window.location.href = `question${ran}.php?score=<?php echo $score ?>&count=<?php echo $count + 1 ?>`;'>Chicago</button>
+            </div>
+        </div>
+
+        <script>
+            var buttons = document.querySelectorAll('click');
+            const correct = document.querySelectorAll('correct');
+
+            let clicked = () => {
+                var correctButton = document.getElementById('correct');
+                correctButton.style.backgroundColor = "green";
+                correctButton.style.color = "white";
+                window.location.href = 'question6.php?score=<?php echo $score ?>'; 
+            }
+
+            let addScore = () => {
+                var correctButton = document.getElementById('correct');
+                correctButton.style.backgroundColor = "green";
+                correctButton.style.color = "white";
+                var ran = Math.ceil(Math.random() * 50);
+                window.location.href = `question${ran}.php?score=<?php echo $score + 1 ?>&count=<?php echo $count + 1 ?>`; 
+            }
+
+            
+        </script>
+    </body>
+</html>
---
+++ systematic/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Quiz game</title>
+
+        <!-- add styles -->
+        <style>
+            *{
+                margin:0;
+                top:0;
+            }
+
+            body{
+                background-color: rgba(11, 95, 219, 0.973);
+            }
+
+            a{
+                text-decoration: none;
+            }
+
+            .nav{
+                display: flex;
+                justify-content: space-between;
+                padding-top:12px;
+                font-family: Arial, helvetica;
+            }
+
+            .nav .logo{
+                padding-left: 12px;
+            }
+
+            .nav .logo span{
+                color: purple;
+            }
+
+            ul{
+               list-style: none; 
+               display:flex;
+            }
+
+            ul li{
+                padding-left:8px;
+                padding-right:8px;
+            }
+
+            ul li a{
+                text-decoration: none;
+                text-align: center;
+                padding:2px 4px;
+                font-family: cursive;
+                font-size:18px;
+                color: #333;
+                transition: .4s ease-in-out;
+            }
+
+            ul li a:hover{
+                color: purple;
+            }
+
+            .game-body .intro-heading, .game-body .outro-heading{
+                font-family: cursive;
+                font-size:23px;
+                text-align: center;
+                padding-top:18px;
+                margin:23px auto 12px;
+                color: #fff;
+            }
+
+            .game-body .columns{
+                display: flex;
+                flex-direction: column;
+            }
+
+            .game-body .columns .row{
+                max-width: 50%;
+                width: 50% ;
+                height: 200px;
+                margin: 12px auto;
+                padding: 5px 8px;
+                text-align: center;
+                border-radius: 20px;
+                box-shadow: 1px 2px #334;
+            }
+
+            .row h2{
+                font-weight: 700;
+                font-family: Arial, helvetica;
+                letter-spacing: 2px;
+            }
+
+            .pro{
+                background-color: #333;
+                color: #fff;
+            }
+
+            .standard{
+                background-color: #fff;
+                color: #333;
+            }
+
+            .audio{
+                top:0;
+                margin-bottom: 20px;
+            }
+        </style>
+    </head>
+    <body>
+
+        <!-- Code for the navigation bar -->
+        <!-- <div class="nav">
+            <div class="logo">
+                <h1>t<span>Quiz</span></h1>
+            </div>
+            <div class="navbar">
+                <ul>
+                    <li><a href="#">New Game</a></li>
+                    <li><a href="#">ScoreBoards</a></li>
+                    <li><a href="#">Info</a></li>
+                </ul>
+            </div>
+        </div> -->
+
+        <!-- Code for the main body -->
+        <div class="game-body">
+            <div class="intro-heading">
+                    <div class="audio" hidden>
+                        <audio controls autoplay>
+                            <source src="92-Rihanna feat Drake - Work(DjDeo V.Xtendz).mp3" type="audio/mpeg">
+                            Your browser does not support the audio element.
+                        </audio>
+                    </div>
+                    <h1>SELECT GAME MODE</h1>
+            </div>
+            
+            <div class="columns">
+                <a href='../standard/question<?php echo rand(0,50) ?>.php?score=<?php echo 0; ?>&count=<?php echo 1; ?>'>
+                    <div class="row standard">
+                        <h2 style="font-weight:800;font-size:28px;">Random</h2>
+                        <h2 style="padding-top:25px;"> - Practice mode<br> - Completely random<br> - Repetitive problems<br> - Answer as many questions as possible </h2>
+
+                    </div>
+                </a>
+
+                <a href="question1.php?score=<?php echo 0; ?>&count=<?php echo 1; ?>">
+                    <div class="row pro">
+                        <h2 style="font-weight:800;font-size:28px;">Systematic</h2>
+                        <h2 style="padding-top:25px;"> - Test mode<br> - Completely systematic<br> - Non-repetitive problems<br> - Sit for a test with 50 questions </h2>
+                    </div>
+                </a>
+            </div>
+
+            <div class="outro-heading">
+            <h1><a href="help.html" style="text-decoration:none;color:white;">Need help?</a></h1>
+            </div>
+        </div>
+    </body>
+</html>
